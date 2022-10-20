7e1e3cb97c65d43d1a15fa785037a6b6a2dc4c63
Salvatore-Gianfrancesco <salvatore.gianfrancesco@yahoo.it>
2022-10-20 14:56:20 +0200

    added visualization in DOM and in console

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,23 +7,60 @@ const team = [
     {
         name: "Angelo Caroll",
         role: "Chief Editor",
-        image: "../img/angela-caroll-chief-editor.jpg"
-    }, {
+        image: "angela-caroll-chief-editor.jpg"
+    },
+    {
         name: "Walter Gordon",
         role: "Office Manager",
         image: "walter-gordon-office-manager.jpg"
-    }, {
+    },
+    {
         name: "Angela Lopez",
         role: "Social Media Manager",
         image: "angela-lopez-social-media-manager.jpg"
-    }, {
+    },
+    {
         name: "Scott Estrada",
         role: "Developer",
         image: "scott-estrada-developer.jpg"
-    }, {
+    },
+    {
         name: "Barbara Ramos",
         role: "Graphic Designer",
         image: "barbara-ramos-graphic-designer.jpg"
     },
 ]
 // console.log(team);
+
+for (let i = 0; i < team.length; i++) {
+    for (const key in team[i]) {
+        console.log(key + ": " + team[i][key]);
+    }
+    console.log("");
+}
+
+const rowEl = document.querySelector(".row");
+// console.log(rowEl);
+
+/* Creazione delle colonne nelle quali inserire le card */
+for (let i = 0; i < team.length; i++) {
+    const colEl = document.createElement("div");
+    colEl.classList.add(".col");
+    // console.log(colEl);
+
+    rowEl.insertAdjacentElement("beforeend", colEl);
+}
+
+const colEl = document.getElementsByClassName(".col");
+// console.log(colEl);
+for (let i = 0; i < team.length; i++) {
+    const cardMarkup = `
+        <div class="card border-0 rounded-0">
+            <img src="./assets/img/${team[i].image}" alt="">
+            <h3 class="my-2">${team[i].name}</h3>
+            <p class="fs-5">${team[i].role}</p>
+        </div>
+    `
+    const currentCard = colEl[i];
+    currentCard.innerHTML = cardMarkup;
+}
